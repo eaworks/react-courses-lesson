91513a22409d75f4c8faa1e7a126984af41f85a9
Ersin Aydın <ersinaydinworks@gmail.com>
2023-03-24 19:36:14 +0300

    get axios data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import './App.css';
+import Courses from './Courses';
+
 
 function App() {
+  const [courses, setCourses] = useState()
+  const fetchCourses = async () => {
+    const response = await axios.get('http://localhost:3000/courses');
+    setCourses(response.data)
+    debugger;
+  }
+  useEffect(() => {
+    fetchCourses();
+  }, [])
+
   return (
     <div className="App">
+      <Courses courses={courses} />
     </div>
   );
 }
